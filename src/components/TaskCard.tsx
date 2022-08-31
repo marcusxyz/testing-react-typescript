912b6c750af862e37d106112b95d69aafd85227d
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-08-31 14:41:16 +0200

    Functionality for task completed

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Todo } from './model';
 import { MdCheck, MdDelete, MdEdit } from 'react-icons/md';
 
+import './styles.css';
+
 // Testing type just to get the hang of it.
 type TaskProps = {
   todo: Todo;
@@ -10,18 +12,31 @@ type TaskProps = {
 };
 
 const TaskCard: React.FC<TaskProps> = ({ todo, lists, setLists }) => {
+  const handleDone = (id: number) => {
+    setLists(
+      lists.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
   return (
     <form className='task__single'>
-      <span className='task__single--desciption'>{todo.desciption}</span>
-      <div className=''>
+      {/* // If todo is done(?) show striked text. Else (:) Show text */}
+      {todo.isDone ? (
+        <s className='task__single--description'>{todo.desciption}</s>
+      ) : (
+        <span className='task__single--description'>{todo.desciption}</span>
+      )}
+      <div className='container'>
         <span className='icon'>
           <MdEdit />
         </span>
-        <span className='icon'>
-          <MdDelete />
+        <span className='icon' onClick={() => handleDone(todo.id)}>
+          <MdCheck />
         </span>
         <span className='icon'>
-          <MdCheck />
+          <MdDelete />
         </span>
       </div>
     </form>
